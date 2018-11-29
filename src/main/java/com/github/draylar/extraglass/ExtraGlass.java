693aee3c86e14f750bb4abe38990ac2d015e5894
Draylar <samuelhimes@gmail.com>
2018-11-29 16:29:13 -0600

    Added localization for darkened incorporeal glass

--- src/main/java/com/github/draylar/extraglass/ExtraGlass.java
+++ src/main/java/com/github/draylar/extraglass/ExtraGlass.java
@@ -5,12 +5,12 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.event.RegistryEvent;
 
 @Mod(
         modid = ExtraGlass.MOD_ID,
