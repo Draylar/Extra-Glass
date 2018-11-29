0b26e6baf77d328316c5b4613a1637154fc2e96a
Draylar <samuelhimes@gmail.com>
2018-11-29 16:54:47 -0600

    Added recipes for stained darkened incorporeal glass

--- src/main/java/com/github/draylar/extraglass/ExtraGlass.java
+++ src/main/java/com/github/draylar/extraglass/ExtraGlass.java
@@ -22,7 +22,7 @@ public class ExtraGlass
 
     public static final String MOD_ID = "extraglass";
     public static final String MOD_NAME = "Extra Glass";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     /**
      * This is the instance of your mod as created by Forge. It will never be null.
